6721b704ccefcdace8f423a50ac5210e34938674
Celia Ortega <celia.indicia@gmail.com>
2024-12-31 10:24:40 +0000

    models exercise

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,17 +1,30 @@
-{{ config(materialized='table') }}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
 
-with payments as (
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+order_payments as (
     select
         order_id,
-        amount
-    from {{ ref('stg_stripe__payments') }}
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
 ),
-orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
-select
-    orders.order_id,
-    orders.customer_id,
-    coalesce(payments.amount, 0) as amount
-from orders
-left join payments using (order_id)
+
+select * from final
